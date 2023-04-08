49ecdc99a6bf070c8769a1754b28c1865327ffcf
jsapas <jaanussapas@gmail.com>
2023-04-08 22:21:07 +0300

    add new train script

--- 01_test_spec.py
+++ 01_test_spec.py
@@ -207,7 +207,7 @@ if __name__ == "__main__":
             y_pred = [0. for k in test_files]
             for file_idx, file_path in tqdm(enumerate(test_files), total=len(test_files)):
                 try:
-                    data = com.file_to_vector_array(file_path,
+                    data = com.file_to_vector_array_spec(file_path,
                                                     n_mels=param["feature"]["n_mels"],
                                                     frames=param["feature"]["frames"],
                                                     n_fft=param["feature"]["n_fft"],
