cd3154a8ef5b6a7c738af378ee01218d0ca90f7e
jsapas <jaanussapas@gmail.com>
2023-04-08 20:52:01 +0300

    change dims

--- 00_train.py
+++ 00_train.py
@@ -3,6 +3,8 @@
  @brief  Script for training
  @author Toshiki Nakamura, Yuki Nikaido, and Yohei Kawaguchi (Hitachi Ltd.)
  Copyright (C) 2020 Hitachi, Ltd. All right reserved.
+
+ modified by jsapas
 """
 
 ########################################################################
@@ -78,45 +80,6 @@ class visualizer(object):
 ########################################################################
 
 
-def list_to_vector_array(file_list,
-                         msg="calc...",
-                         n_mels=64,
-                         frames=5,
-                         n_fft=1024,
-                         hop_length=512,
-                         power=2.0):
-    """
-    convert the file_list to a vector array.
-    file_to_vector_array() is iterated, and the output vector array is concatenated.
-
-    file_list : list [ str ]
-        .wav filename list of dataset
-    msg : str ( default = "calc..." )
-        description for tqdm.
-        this parameter will be input into "desc" param at tqdm.
-
-    return : numpy.array( numpy.array( float ) )
-        vector array for training (this function is not used for test.)
-        * dataset.shape = (number of feature vectors, dimensions of feature vectors)
-    """
-    # calculate the number of dimensions
-    dims = n_mels * frames
-
-    # iterate file_to_vector_array()
-    for idx in tqdm(range(len(file_list)), desc=msg):
-        vector_array = com.file_to_vector_array_spec(file_list[idx],
-                                                n_mels=n_mels,
-                                                frames=frames,
-                                                n_fft=n_fft,
-                                                hop_length=hop_length,
-                                                power=power)
-        if idx == 0:
-            dataset = numpy.zeros((vector_array.shape[0] * len(file_list), dims), float)
-        dataset[vector_array.shape[0] * idx: vector_array.shape[0] * (idx + 1), :] = vector_array
-
-    return dataset
-
-
 def file_list_generator(target_dir,
                         dir_name="train",
                         ext="wav"):
@@ -184,7 +147,7 @@ if __name__ == "__main__":
         # generate dataset
         print("============== DATASET_GENERATOR ==============")
         files = file_list_generator(target_dir)
-        train_data = list_to_vector_array(files,
+        train_data = com.list_to_vector_array_spec(files,
                                           msg="generate train_dataset",
                                           n_mels=param["feature"]["n_mels"],
                                           frames=param["feature"]["frames"],
--- common.py
+++ common.py
@@ -120,6 +120,52 @@ def file_load(wav_name, mono=False):
 ########################################################################
 # feature extractor
 ########################################################################
+def file_to_vector_array(file_name,
+                         n_mels=64,
+                         frames=5,
+                         n_fft=1024,
+                         hop_length=512,
+                         power=2.0):
+    """
+    convert file_name to a vector array.
+
+    file_name : str
+        target .wav file
+
+    return : numpy.array( numpy.array( float ) )
+        vector array
+        * dataset.shape = (dataset_size, feature_vector_length)
+    """
+    # 01 calculate the number of dimensions
+    dims = n_mels * frames
+
+    # 02 generate melspectrogram using librosa
+    y, sr = file_load(file_name)
+
+    mel_spectrogram = librosa.feature.melspectrogram(y=y,
+                                                     sr=sr,
+                                                     n_fft=n_fft,
+                                                     hop_length=hop_length,
+                                                     n_mels=n_mels,
+                                                     power=power)
+
+    # 03 convert melspectrogram to log mel energy
+    log_mel_spectrogram = 20.0 / power * numpy.log10(mel_spectrogram + sys.float_info.epsilon)
+
+    # 04 calculate total vector size
+    vector_array_size = len(log_mel_spectrogram[0, :]) - frames + 1
+
+    # 05 skip too short clips
+    if vector_array_size < 1:
+        return numpy.empty((0, dims))
+
+    # 06 generate feature vectors by concatenating multiframes
+    vector_array = numpy.zeros((vector_array_size, dims))
+    for t in range(frames):
+        vector_array[:, n_mels * t: n_mels * (t + 1)] = log_mel_spectrogram[:, t: t + vector_array_size].T
+
+    return vector_array
+
 def file_to_vector_array_spec(file_name,
                          n_mels=64,
                          frames=5,
@@ -137,24 +183,12 @@ def file_to_vector_array_spec(file_name,
         * dataset.shape = (dataset_size, feature_vector_length)
     """
     # 01 calculate the number of dimensions
-    #dims = n_mels * frames
     n = (n_fft//2 + 1)
-    dims = (n_fft//2 + 1) * frames
+    dims = n * frames
 
     # 02 generate melspectrogram using librosa
     y, sr = file_load(file_name)
 
-    '''
-    mel_spectrogram = librosa.feature.melspectrogram(y=y,
-                                                     sr=sr,
-                                                     n_fft=n_fft,
-                                                     hop_length=hop_length,
-                                                     n_mels=n_mels,
-                                                     power=power)
-
-    # 03 convert melspectrogram to log mel energy
-    log_mel_spectrogram = 20.0 / power * numpy.log10(mel_spectrogram + sys.float_info.epsilon)
-    '''
     D = np.abs(librosa.stft(y, n_fft=n_fft,  hop_length=hop_length))
     log_spectrogram = 20.0 / power * np.log10(D + sys.float_info.epsilon)
 
@@ -230,6 +264,45 @@ def list_to_vector_array(file_list,
     """
     # calculate the number of dimensions
     dims = n_mels * frames
+
+    # iterate file_to_vector_array()
+    for idx in tqdm(range(len(file_list)), desc=msg):
+        vector_array = file_to_vector_array(file_list[idx],
+                                            n_mels=n_mels,
+                                            frames=frames,
+                                            n_fft=n_fft,
+                                            hop_length=hop_length,
+                                            power=power)
+        if idx == 0:
+            dataset = np.zeros((vector_array.shape[0] * len(file_list), dims), float)
+            logger.info((f'Creating data for {len(file_list)} files: size={dataset.shape[0]}'
+                         f', shape={dataset.shape[1:]}'))
+        dataset[vector_array.shape[0] * idx: vector_array.shape[0] * (idx + 1), :] = vector_array
+
+    return dataset
+
+def list_to_vector_array_spec(file_list,
+                         msg="calc...",
+                         n_mels=64,
+                         frames=5,
+                         n_fft=1024,
+                         hop_length=512,
+                         power=2.0):
+    """
+    convert the file_list to a vector array.
+    file_to_vector_array() is iterated, and the output vector array is concatenated.
+    file_list : list [ str ]
+        .wav filename list of dataset
+    msg : str ( default = "calc..." )
+        description for tqdm.
+        this parameter will be input into "desc" param at tqdm.
+    return : np.array( np.array( float ) )
+        vector array for training (this function is not used for test.)
+        * dataset.shape = (number of feature vectors, dimensions of feature vectors)
+    """
+    # calculate the number of dimensions
+    n = (n_fft//2 + 1)
+    dims = n * frames
 
     # iterate file_to_vector_array()
     for idx in tqdm(range(len(file_list)), desc=msg):
