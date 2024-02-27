118e39c3548712bde381c055d7de90e70b326139
jsapas <jaanussapas@gmail.com>
2024-02-27 23:07:07 +0200

    edit logging

--- common.py
+++ common.py
@@ -40,11 +40,7 @@ Standard output is logged in "train.log".
 """
 import logging
 
-logger = logging.getLogger('common')
-handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+logger = logging.getLogger(__name__)
 
 
 ########################################################################
